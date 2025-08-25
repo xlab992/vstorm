9a987cc05041ae281b18c0a9fa961691be429d5a
qwertyuiop8899 <emanuele.schiano+vix@gmail.com>
2025-08-25 09:14:07 +0200

    Update addon.ts

--- src/addon.ts
+++ src/addon.ts
@@ -471,9 +471,9 @@ function decodeStaticUrl(url: string): string {
 // ================= MANIFEST BASE (restored) =================
 const baseManifest: Manifest = {
     id: "org.stremio.vixcloud",
-    version: "5.3.2",
+    version: "5.5.23",
     name: "StreamViX",
-    description: "Addon for Vixsrc, Anime providers and Live TV.",
+    description: "Addon for Vixsrc, Anime providers Live TV and Events.",
     background: "https://raw.githubusercontent.com/qwertyuiop8899/StreamViX/refs/heads/main/public/backround.png",
     types: ["movie", "series", "tv", "anime"],
     idPrefixes: ["tt", "kitsu", "tv", "mal", "tmdb"],
