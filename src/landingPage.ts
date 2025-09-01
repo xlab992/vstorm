cd55117039c64fff011d0fc9d801c00e6a1dd84e
qwertyuiop8899 <emanuele.schiano+vix@gmail.com>
2025-09-01 14:53:22 +0200

    Update landingPage.ts

--- src/landingPage.ts
+++ src/landingPage.ts
@@ -295,8 +295,12 @@ function landingTemplate(manifest: any) {
 					}
 					if (toggleMap[key]) {
 						const t = toggleMap[key];
-						// All default ON visually: mark as checked
-						const checkedAttr = ' checked';
+						// Determine checked state from elem.default (boolean) if provided; default visually ON
+						const hasDefault = (typeof (elem as any).default === 'boolean');
+						const rawDefault = hasDefault ? !!(elem as any).default : true; // underlying value
+						// If invert=true (disable flags), invert the visual checked state
+						const isChecked = t.invert ? !rawDefault : rawDefault;
+						const checkedAttr = isChecked ? ' checked' : '';
 						options += `
 						<div class="form-element">
 							<div class="toggle-row" data-toggle-row="${key}">
@@ -313,7 +317,10 @@ function landingTemplate(manifest: any) {
 						</div>
 						`
 					} else {
-						const isChecked = elem.default === 'checked' ? ' checked' : ''
+						// Support boolean default as well as legacy 'checked'
+						const isChecked = (typeof (elem as any).default === 'boolean')
+							? (((elem as any).default as boolean) ? ' checked' : '')
+							: (elem.default === 'checked' ? ' checked' : '')
 						options += `
 						<div class="form-element">
 							<label for="${key}">
