250dafbfdc0e6d7de176802730b4803b34831c6d
qwertyuiop8899 <emanuele.schiano+vix@gmail.com>
2025-09-06 14:11:48 +0200

    Update guardahd-provider.ts

--- src/providers/guardahd-provider.ts
+++ src/providers/guardahd-provider.ts
@@ -1,7 +1,7 @@
 //# thanks to @urlomythus for the code
 //#Adapted for use in Streamvix from:
 //# Mammamia  in https://github.com/UrloMythus/MammaMia
-//# 
+//# thanks to @webstreamr for the code Mammamia  in https://github.com/webstreamr/webstreamr
 
 /** GuardaHD Provider (raw HLS, no proxy) - single clean implementation */
 import type { StreamForStremio } from '../types/animeunity';
