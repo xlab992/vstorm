179283c8a4aded3451e1e29dad8fbf4065ef172b
qwertyuiop8899 <emanuele.schiano+vix@gmail.com>
2025-09-07 16:57:56 +0200

    fix: deltabit priority

--- src/providers/eurostreaming-provider.ts
+++ src/providers/eurostreaming-provider.ts
@@ -1,4 +1,4 @@
-// thanks @urlomithus for the code https://github.com/UrloMythus/MammaMia
+// thanks @urlomithus for the code https://github.com/UrloMythus/MammaMia !
 import { spawn } from 'child_process';
 import * as path from 'path';
 import type { StreamForStremio } from '../types/animeunity';
