650d37af2860caf1be499a0c1fa050dbc690581d
qwertyuiop8899 <emanuele.schiano+vix@gmail.com>
2025-09-17 11:06:06 +0200

    Update addon.ts

--- src/addon.ts
+++ src/addon.ts
@@ -482,7 +482,7 @@ function decodeStaticUrl(url: string): string {
 // ================= MANIFEST BASE (restored) =================
 const baseManifest: Manifest = {
     id: "org.stremio.vixcloud",
-    version: "7.4.23",
+    version: "7.5.23",
     name: "StreamViX | Elfhosted",
     description: "StreamViX addon con Vixsrc, Guardaserie, Altadefinizione, AnimeUnity, AnimeSaturn, AnimeWorld, Eurostreaming, TV ed Eventi Live",
     background: "https://raw.githubusercontent.com/qwertyuiop8899/StreamViX/refs/heads/main/public/backround.png",
